149cab7ccd346a439ecd08109e9948e616542ac5
Jan Arild Bjørnsson <janab@ifi.uio.no>
2024-01-05 16:11:49 +0100

    add pawn move check

--- chess/Pawn.java
+++ chess/Pawn.java
@@ -1,3 +1,5 @@
+import java.lang.Math;
+
 public class Pawn extends ChessPiece {
 
     public Pawn(ChessColor c) {
@@ -5,7 +7,39 @@ public class Pawn extends ChessPiece {
     }
 
     public boolean isLegalMove(Move selectedMove) {
-        return true;
+        int xMovement = selectedMove.getOrgSquare().getXPos() - selectedMove.getDestSquare().getXPos();
+        int yMovement = selectedMove.getOrgSquare().getYPos() - selectedMove.getDestSquare().getYPos();
+        ChessPiece movingPiece = selectedMove.getMovingPiece();
+        ChessPiece destPiece = selectedMove.getDestSquare().getPiece();
+        if (movingPiece.getColor() == ChessColor.WHITE) {
+            if (destPiece != null && destPiece.getColor() == ChessColor.BLACK && 
+            Math.abs(xMovement) == 1 && yMovement == -1) {
+                return true;
+            }
+            else if (destPiece == null && xMovement == 0 &&
+            yMovement == -2 && selectedMove.getOrgSquare().getYPos() == 2) {
+                return true;
+            }
+            else if (destPiece == null && xMovement == 0 &&
+            yMovement == -1) {
+                return true;
+            }
+        }
+        if (movingPiece.getColor() == ChessColor.BLACK) {
+            if (destPiece != null && destPiece.getColor() == ChessColor.WHITE && 
+            Math.abs(xMovement) == 1 && yMovement == 1) {
+                return true;
+            }
+            else if (destPiece == null && xMovement == 0 &&
+            yMovement == 2 && selectedMove.getOrgSquare().getYPos() == 7) {
+                return true;
+            }
+            else if (destPiece == null && xMovement == 0 &&
+            yMovement == 1) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public String toString() {
